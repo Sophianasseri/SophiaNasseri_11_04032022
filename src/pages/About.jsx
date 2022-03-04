4dd6b37ef615ff404408aab18eb451da1a0d0815
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-03-04 15:57:56 +0100

    Add pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <div>
+      <h1>À propos</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Accomodation.jsx
@@ -0,0 +1,9 @@
+const Accomodation = () => {
+  return (
+    <div>
+      <h1>Logement</h1>
+    </div>
+  );
+};
+
+export default Accomodation;
