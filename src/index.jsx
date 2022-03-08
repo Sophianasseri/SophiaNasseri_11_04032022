716d1e34c2a3ad4adba13cf3fa6920cfa36ec7aa
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-03-08 19:05:20 +0100

    Change router structure to be compatible with react router v6

--- src/index.jsx
+++ src/index.jsx
@@ -1,30 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './index.css';
-import Home from './pages/Home';
-import About from './pages/About';
-import Accommodation from './pages/Accomodation';
-import NotFound from './pages/NotFound';
+import './styles/index.scss';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/accommodation">
-          <Accommodation />
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
