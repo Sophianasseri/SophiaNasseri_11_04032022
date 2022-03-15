7bbd8183a6385a99e3d0caa074a04364c1c6aaf3
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-03-15 14:20:45 +0100

    change dropdown data into a json file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Banner from '../components/Banner/Banner';
 import aboutBanner from '../assets/aboutBanner.png';
-import dropdownData from '../data/dropdownData';
+import dropdownData from '../data/dropdown.json';
 import Dropdown from '../components/Dropdown/Dropdown';
 
 function About() {
