f3351b6cdb03f8a370f620c24936b48b40204fc5
Sophia Nasseri <nasseri.sophia@gmail.com>
2022-03-18 18:32:59 +0100

    :feat: redirection to 404 error page if wrong id parameter

--- src/pages/Accommodation/Accommodation.jsx
+++ src/pages/Accommodation/Accommodation.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
 import Carousel from '../../components/Carousel/Carousel';
 import Dropdown from '../../components/Dropdown/Dropdown';
 import Host from '../../components/Host/Host';
@@ -10,56 +10,67 @@ import styles from './accommodation.module.css';
 
 function Accomodation() {
   const { id } = useParams();
+  const [accommodation, setAccommodation] = useState(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accommodation === undefined) {
+      const element = data.find((elt) => elt.id === id);
+      /* Redirect to page error 404 if user change id parameter to an undefined one */
+      if (element === undefined) {
+        navigate('/404');
+      } else {
+        setAccommodation(element);
+      }
+    }
+  }, []);
   return (
     <main>
-      {data
-        .filter((elt) => elt.id === id)
-        .map((accommodation) => (
-          <div key={accommodation.id}>
-            <section>
-              <Carousel imageArr={accommodation.pictures} />
-            </section>
-            <section>
-              <div className={styles.info}>
-                <div>
-                  <h2 className={styles.title}>{accommodation.title}</h2>
-                  <p className={styles.location}>{accommodation.location}</p>
-                  <ul className={styles.tag}>
-                    {accommodation.tags.map((tag) => (
-                      <Tags key={tag} content={tag} />
-                    ))}
-                  </ul>
-                </div>
-                <div>
-                  <div className={styles.host}>
-                    <Host
-                      name={accommodation.host.name}
-                      imageScr={accommodation.host.picture}
-                    />
-                  </div>
-                  <Rating rateValue={accommodation.rating} />
-                </div>
-              </div>
-
-              <div className={styles.dropdown}>
-                <Dropdown
-                  title="Description"
-                  content={<p>{accommodation.description}</p>}
-                  smallDropdown
-                />
-                <Dropdown
-                  title="Équipements"
-                  content={accommodation.equipments.map((eq) => (
-                    <ul key={eq.id}>
-                      <li>{eq}</li>
-                    </ul>
+      {accommodation ? (
+        <div key={accommodation.id}>
+          <section>
+            <Carousel imageArr={accommodation.pictures} />
+          </section>
+          <section>
+            <div className={styles.info}>
+              <div>
+                <h2 className={styles.title}>{accommodation.title}</h2>
+                <p className={styles.location}>{accommodation.location}</p>
+                <ul className={styles.tag}>
+                  {accommodation.tags.map((tag) => (
+                    <Tags key={tag} content={tag} />
                   ))}
-                  smallDropdown
-                />
+                </ul>
               </div>
-            </section>
-          </div>
-        ))}
+              <div>
+                <div className={styles.host}>
+                  <Host
+                    name={accommodation.host.name}
+                    imageScr={accommodation.host.picture}
+                  />
+                </div>
+                <Rating rateValue={accommodation.rating} />
+              </div>
+            </div>
+
+            <div className={styles.dropdown}>
+              <Dropdown title="Description" smallDropdown>
+                <p>{accommodation.description}</p>
+              </Dropdown>
+
+              <Dropdown title="Équipements" smallDropdown>
+                <ul>
+                  {accommodation.equipments.map((eq) => (
+                    <li key={eq.id}>{eq}</li>
+                  ))}
+                </ul>
+              </Dropdown>
+            </div>
+          </section>
+        </div>
+      ) : (
+        <p>Chargement...</p>
+      )}
     </main>
   );
 }
